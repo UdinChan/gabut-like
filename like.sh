bb01853e4f359e6dc9c7446cff0c7829c5481350
Jaenudin Maulana Marpaung <resultlegal69@gmail.com>
2019-09-03 17:38:20 +0700

    Update like.sh

--- like.sh
+++ like.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # Nikmati aja gayn
+
+# User-Agent List
+UA=$(cat ua.txt | sort -R  | head -1)
+
+# Colours
 DG='\033[1;32m'
 NC='\033[0m'
 CYAN='\e[38;5;45m'
 RED='\e[38;5;196m'
 GREEN='\e[38;5;82m'
+
+# ID Token
 TOKEN=""
 ID=""
+
+# run get.sh
 bash get.sh
-cekvoc(){
-curl -A "Chrome" -s "https://gabut.club/api/v3-beta/method/items.like" \
+
+like(){
+curl -A "${UA}" -s "https://gabut.club/api/v3-beta/method/items.like" \
 --data "accessToken=${TOKEN}&accountId=${ID}&itemId=${itemId}&" | grep -Po 'myLike":true'>
 if [[ $ppk != false ]]; then
 echo -e "${CYAN}$itemId${NC} = ${GREEN}Liked${NC}"
@@ -18,24 +28,24 @@ echo -e "${CYAN}$itemId${NC} = ${GREEN}Already Liked${NC}"
 fi
 }
 
-gas(){
+doLike(){
 NOW=`date "+%r"`
 
-for itemId in `cat $voc`
+for itemId in `cat $item`
 do
-cekvoc $itemId
+like $itemId
 done
 rm id.txt
 }
 
 cek(){
-if [ ! -f $voc ]; then
-echo "File $voc Not Found"
+if [ ! -f $item ]; then
+echo "File $item Not Found"
 exit
 else
-gas
+doLike
 fi
 }
 
-voc='id.txt'
+item='id.txt'
 cek
