b21cf6c3b52c07a6ce5d52b97ec3f16e42d067bd
Jaenudin Maulana Marpaung <resultlegal69@gmail.com>
2019-09-03 17:33:24 +0700

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+# User-Agent List
+UA=$(cat ua.txt | sort -R  | head -1)
+
+# Username Password
 USER="username"
 PASS="password"
+
+# Login
 login(){
-local log=$(curl -s -k -A "Dalvik" "https://gabut.club/api/v3-beta/method/account.signIn">
+local log=$(curl -s -k -A "${UA}" "https://gabut.club/api/v3-beta/method/account.signIn">
 -d "username=${USER}&clientId=1005&password=${PASS}")
 local tok=$(echo -e "${log}" | grep -Po '(?<=\"accessToken\":").*?(?=")')
 local id=$(echo -e "${log}" | grep -Po '(?<=\"accountId\":").*?(?=")')
